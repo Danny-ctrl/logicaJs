c823984431b81d8aa40ec35d99779e98d59657f3
Daniele Inspiron <contatodanny@hotmail.com>
2025-01-23 01:21:47 -0300

    desafio

---
+++ desafio_logica/app.js
@@ -0,0 +1,12 @@
+alert('Boas vindas ao nosso site!');
+let nome ='Lua';
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+alert('Erro!Preencha todos os campos');
+let mensagemDeErro ='Erro!Preencha todos os campos';
+alert(mensagemDeErro);
+let suaIdade =prompt('Digite sua idade');
+if(suaIdade>=18){
+  alert('Pode tirar a habilitação!')
+}
