73ab1f0cbdaadd490827e647662cd659017c056d
Daniele Inspiron <contatodanny@hotmail.com>
2025-01-23 00:40:46 -0300

    first commit

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,6 @@
+alert('Boas vindas ao jogo do número secreto');
+let numeroSecreto =15;
+let chute =prompt('Escolha um número entre 1 e 20');
+if(chute == numeroSecreto){
+  console.log('Isso ai!Você acertou o número secreto(5)');
+}
