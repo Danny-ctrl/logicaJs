79647501740660432006f8d08bd1c3f252e37344
Daniele Inspiron <contatodanny@hotmail.com>
2025-01-23 01:44:30 -0300

    else

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -2,5 +2,7 @@ alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto =15;
 let chute =prompt('Escolha um número entre 1 e 20');
 if(chute == numeroSecreto){
-  console.log('Isso ai!Você acertou o número secreto(5)');
+  alert('Isso ai!Você acertou o número secreto '+ numeroSecreto);
+}else{
+  alert('Você errou :(')
 }
