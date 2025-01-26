1f5d7f3277e7a425de6c4eb1a4670586954e26b6
Daniele Inspiron <contatodanny@hotmail.com>
2025-01-26 01:10:52 -0300

    desafio 2

---
+++ desafio2_logica/app.js
@@ -0,0 +1,11 @@
+let dia = prompt('Qual dia da semana é hoje?');
+let sabado = 'sabado';
+let domingo = 'domingo';
+if (dia  == sabado ){
+alert('Bom fim de semana');
+}
+if(dia==domingo){
+  alert('Bom fim de semana');
+}else{
+  alert('Boa semana');
+}
