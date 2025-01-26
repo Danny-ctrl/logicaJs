8ba31db1290d4eed8ce1030917c3a90eed6cc158
Daniele Inspiron <contatodanny@hotmail.com>
2025-01-26 03:09:03 -0300

    while

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,8 +1,17 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto =15;
-let chute =prompt('Escolha um número entre 1 e 20');
+let chute ;
+
+while(chute != numeroSecreto){
+  chute=prompt('Escolha um número entre 1 e 20');
+
 if(chute == numeroSecreto){
   alert('Isso ai!Você acertou o número secreto '+ numeroSecreto);
 }else{
-  alert('Você errou :(')
+  if(numeroSecreto > chute){
+alert('O número secreto é maior que '+ chute);
+  }else{
+    alert('O numero secreto é menor que '+ chute);
+  }
+}
 }
