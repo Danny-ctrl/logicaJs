e1f7a824ea0a022bae9aa1eed3fc42e7f28c0a41
Daniele Inspiron <contatodanny@hotmail.com>
2025-01-26 01:25:44 -0300

    nome

--- desafio2_logica/app.js
+++ desafio2_logica/app.js
@@ -1,11 +1,22 @@
-let dia = prompt('Qual dia da semana é hoje?');
+let nome=prompt('Qual seu nome?')
+let dia = prompt(nome +' qual dia da semana é hoje?');
 let sabado = 'sabado';
 let domingo = 'domingo';
+
 if (dia  == sabado ){
 alert('Bom fim de semana');
-}
-if(dia==domingo){
+}else if(dia==domingo){
   alert('Bom fim de semana');
 }else{
   alert('Boa semana');
+}
+
+let numeroDigitado = prompt('Digite um numero');
+if(numeroDigitado >0){
+  alert('Numero positivo');
+}else if(numeroDigitado<0){
+  alert('Numero negativo');
+}
+if(numeroDigitado==0){
+  alert('o número é 0');
 }
